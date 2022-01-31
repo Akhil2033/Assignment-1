aa42d30d5ca54afbc212ca4807fc733d7607f840
Akhil2033 <akhilender1@gmail.com>
2022-01-30 22:40:02 -0500

    changes made

--- Controllers/HostController.cs
+++ Controllers/HostController.cs
@@ -17,7 +17,7 @@ namespace WebApiProject1.Controllers
             double TDC = 0.44; // cost per day including taxes
             double total = TDC * id + TotalCost; // cost for extra days beyond a fort night
 
-            return ("1 fortnights at $5.50/FN = $5.50 CAD \n HST 13 % = $0.72 CA Total= $ \n" + total);
+            return ("1 fortnights at $5.50/FN = $5.50 CAD \n HST 13 % = $0.72 CA Total= $ \n" + total+"CAD");
 
         }
 
