11cda2b064bbedb71c3068c7a27bc3a1f03d6f6f
Akhil2033 <akhilender1@gmail.com>
2022-01-30 22:02:49 -0500

    chages have been made!!

--- Controllers/HostController.cs
+++ Controllers/HostController.cs
@@ -11,11 +11,11 @@ namespace WebApiProject1.Controllers
     {
         public String Get(double id)
         {
-            double Cost = 5.50;
-            double Tax = 0.715;
-            double TotalCost = Cost + Tax;
-            double TDC = 0.44;
-            double total = TDC * id + TotalCost;
+            double Cost = 5.50;// cost per fort night before taxes
+            double Tax = 0.715; // 13% tax on cost per fort night
+            double TotalCost = Cost + Tax; // Cost including taxes
+            double TDC = 0.44; // cost per day including taxes
+            double total = TDC * id + TotalCost; // cost for extra days beyond a fort night
 
             return ("1 fortnights at $5.50/FN = $5.50 CAD "+ "HST 13 % = $0.72 CA Total= " + " " + total);
 
